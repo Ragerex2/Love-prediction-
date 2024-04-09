5068eb829d3fe0dfe8aff1f7c5e3c6124de3abfb
Ragerex2 <166437613+Ragerex2@users.noreply.github.com>
2024-04-09 11:11:28 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,1 +1,13 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve login details from the form
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
+    // You can perform further validation and authentication here
+    // For demonstration purposes, let's just echo the received data
+    echo "Username: " . $username . "<br>";
+    echo "Password: " . $password;
+}
+?>
+  
